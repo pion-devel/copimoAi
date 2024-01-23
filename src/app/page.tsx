20424f532b2a41a4b84322e14fc5aa200e2aacff
pion-devel <premiumboxes@outlook.it>
2024-01-23 09:02:00 +0100

    update description info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
             <div className="mx-auto max-w-6xl px-6 lg:px-8">
               <div className="mt-16 flow-root sm:mt-24">
                 <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                  {/* <Image
+                  <Image
                     src="/dashboard-preview.png"
                     alt="product preview"
                     width={1364}
@@ -74,8 +74,8 @@ export default function Home() {
                     quality={100}
                     className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
                   />
-            */}
-                  <CldVideoPlayer width="1620" height="920" src="copimo-demo" />
+
+                  {/**<CldVideoPlayer width="1620" height="920" src="copimo-demo" /> */}
                 </div>
               </div>
             </div>
