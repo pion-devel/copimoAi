301ec054c5ba98ace0ddcbaa29347d954d6204c7
pion-devel <premiumboxes@outlook.it>
2024-01-23 08:46:32 +0100

    update dashboard image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -152,10 +152,10 @@ export default function Home() {
           <div className="mt-16 flow-root sm:mt-24">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
               <Image
-                src="/dashdocments.png"
+                src="/dashLarge.png"
                 alt="uploading preview"
-                width={1720}
-                height={1020}
+                width={1620}
+                height={920}
                 quality={100}
                 className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
               />
