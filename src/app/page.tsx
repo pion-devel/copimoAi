903eeb0e2f343f92cc3b658af77ba76a513e58a1
pion-devel <premiumboxes@outlook.it>
2024-01-25 11:42:33 +0100

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,28 +12,99 @@ import {
   RegisterLink,
   getKindeServerSession,
 } from "@kinde-oss/kinde-auth-nextjs/server";
+
 export default function Home() {
   return (
     <>
-      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+      {/* value proposition section */}
+      <div>
+        <div className="relative isolate">
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+
+          <div>
+            <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-16">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <Image
+                    src="/copimoBanner.png"
+                    alt="product preview"
+                    width={1364}
+                    height={866}
+                    quality={100}
+                    className="rounded-md bg-white   shadow-2xl ring-1 ring-gray-900/10"
+                  />
+
+                  {/**<CldVideoPlayer width="1620" height="920" src="copimo-demo" /> */}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
+            />
+          </div>
+        </div>
+      </div>
+      <MaxWidthWrapper className="mb-12 mt-12 sm:mt-24 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-12 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
           <p className="text-sm font-semibold text-gray-700">
             Provalo ora! È gratuito.
           </p>
         </div>
+
         <h1 className="max-w-4xl mb-6 text-5xl font-bold md:text-6xl lg:text-7xl">
-          Benvenuti su Kopimo,
-          <span className="text-blue-600">il tuo assistente PDF </span>{" "}
-          intelligente.
+          Scopri il potere dello studio intelligente con
+          <span className="text-blue-600"> Kopimo </span>
         </h1>
-        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-          Ora puoi esplorare i PDF in modo più facile. L&apos;IA qui può tirare
-          fuori il succo da documenti lunghi, spiegarti cose complesse, trovare
-          info importanti in un attimo e addirittura darti suggerimenti su
-          domande da fare. Cool, eh?
-        </p>
+        <div className="display: flex">
+          <span>
+            <Image
+              src="/coolemo.png"
+              alt="product preview"
+              width={100}
+              height={100}
+              quality={100}
+            />
+          </span>
+          <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
+            Ora puoi esplorare i PDF in modo più facile. L&apos;IA qui può
+            tirare fuori il succo da documenti lunghi, spiegarti cose complesse,
+            trovare info importanti in un attimo e addirittura darti
+            suggerimenti su domande da fare. Cool, eh?
+          </p>
+          <span>
+            <Image
+              src="/coolemo.png"
+              alt="product preview"
+              width={100}
+              height={100}
+              quality={100}
+            />
+          </span>
+        </div>
+
         {/**
- * 
+ 
  *  <RegisterLink
           className={buttonVariants({
             size: "sm",
@@ -72,7 +143,7 @@ export default function Home() {
                     width={1364}
                     height={866}
                     quality={100}
-                    className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                    className="rounded-md bg-white  shadow-2xl ring-1 ring-gray-900/10"
                   />
 
                   {/**<CldVideoPlayer width="1620" height="920" src="copimo-demo" /> */}
@@ -97,10 +168,10 @@ export default function Home() {
       </div>
 
       {/* Feature section */}
-      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+      <div className="mx-auto max-w-6xl mb-32 mt-12 sm:mt-32">
         <div className="mb-12 px-6 lg:px-8">
           <div className="mx-auto max-w-2xl sm:text-center">
-            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+            <h2 className="mt-2 mb-9 font-bold text-4xl text-gray-900 sm:text-5xl">
               Ecco come funziona
             </h2>
             <p className="mt-4 text-lg text-gray-600">
@@ -148,16 +219,50 @@ export default function Home() {
           </li>
         </ol>
 
-        <div className="mx-auto max-w-6xl px-6 lg:px-8">
-          <div className="mt-16 flow-root sm:mt-24">
-            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-              <Image
-                src="/dashLarge.png"
-                alt="uploading preview"
-                width={1620}
-                height={920}
-                quality={100}
-                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+        <div>
+          <div className="relative isolate">
+            <div
+              aria-hidden="true"
+              className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            >
+              <div
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+                className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              />
+            </div>
+
+            <div>
+              <div className="mx-auto max-w-6xl px-6 lg:px-8">
+                <div className="mt-16 flow-root sm:mt-24">
+                  <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                    <Image
+                      src="/dashLarge.png"
+                      alt="product preview"
+                      width={1364}
+                      height={866}
+                      quality={100}
+                      className="rounded-md bg-white  shadow-2xl ring-1 ring-gray-900/10"
+                    />
+
+                    {/**<CldVideoPlayer width="1620" height="920" src="copimo-demo" /> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div
+              aria-hidden="true"
+              className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            >
+              <div
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+                className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
               />
             </div>
           </div>
@@ -166,7 +271,7 @@ export default function Home() {
         {/* Title features */}
         <div className="mb-12 mt-28 px-6 lg:px-8">
           <div className="mx-auto max-w-2xl sm:text-center">
-            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+            <h2 className="mt-2 mb-9 font-bold text-4xl text-gray-900 sm:text-5xl">
               Kopimo in poche parole
             </h2>
             <p className="mt-4 text-lg text-gray-600">
